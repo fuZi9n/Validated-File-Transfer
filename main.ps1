0a4d11436763a6f383502746c26ec9b23f728165
fuZi9n <haltemedhjalte@gmail.com>
2024-04-23 13:46:45 +0200

    Updates

--- main.ps1
+++ main.ps1
@@ -1,1 +1,8 @@
-Move-Item -Path /home/batman/Documents/file.txt -Destination /home/batman/Documents/GitHub/file.txt
+#Variable for the source folder
+$source = "/home/batman/Documents/file.txt"
+
+#Variable for the destination folder
+$destination = "/home/batman/Documents/GitHub/file.txt"
+
+#This executes the transfer and uses the folders defined in the variables
+Move-Item -Path $source -Destination $destination
