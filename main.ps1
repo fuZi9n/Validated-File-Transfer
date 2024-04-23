1ae39152fb87ebbda6ab7cba2352da90b84c81c5
fuZi9n <haltemedhjalte@gmail.com>
2024-04-23 14:10:05 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -4,12 +4,14 @@ $source = "/home/batman/Documents/file.txt"
 #Variable for the destination folder
 $destination = "/home/batman/Documents/GitHub/file.txt"
 
-#Checks if both the paths are valid
+#Checks if the source file exists
 if (Test-Path $source) {
+    #Sends a message to terminal 
     Write-Host "Path exists"
     
     #This executes the transfer and uses the folders defined in the variables
     Move-Item -Path $source -Destination $destination
+
 }
 else {
     Write-Host "Path does not exist"
