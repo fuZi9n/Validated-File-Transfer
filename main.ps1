c5f3c87383b42c6195e54bcbd9c33b89de1fee63
fuZi9n <haltemedhjalte@gmail.com>
2024-04-23 14:41:34 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,10 +1,10 @@
 #Variable for the source folder
-$source = "/home/batman/Documents/file.txt"
+$source = '/home/batman/Documents/file.txt'
 
 $sourceTest = Test-Path $source
 
 #Variable for the destination folder
-$destination = "/home/batman/Documents/GitHub/file.txt"
+$destination = '/home/batman/Documents/GitHub/file.txt'
 
 $destinationTest = Test-Path $destination
 
@@ -12,5 +12,5 @@ if ($sourceTest -eq $true) {
     Write-Host "The path is valid"
 }
 elseif (($sourceTest -eq $false)) {
-    Write-Host "Path not valid"
+    Write-Host "The path is not valid"
 }
