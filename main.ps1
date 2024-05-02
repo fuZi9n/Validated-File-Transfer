5f057ddc63e5dc0a5a7caa782deb437aafffc357
Hjalte <62058859+fuZi9n@users.noreply.github.com>
2024-05-02 16:36:49 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -4,19 +4,22 @@ $source = '/home/batman/Documents/file.txt'
 $sourceTest = Test-Path $source
 
 #Variable for the destination folder
-$destination = '/home/batman/Documents/GitHub/file.txt'
+$destination = '/home/batman/Documents/GitHub/'
 
 $destinationTest = Test-Path $destination
 
 if ($sourceTest -eq $true) {
-    Write-Host "The source path is valid"
+    Write-Host "The source path is valid, specified file was found"
 }
 elseif (($sourceTest -eq $false)) {
     Write-Host "The source path is not valid, please check path input!"
+    exit
 }
-elseif ($destinationTest -eq $true) {
+
+if($destinationTest -eq $true) {
     Write-Host "The destination path is valid"
 }
 elseif ($destinationTest -eq $false) {
     Write-Host "The destination path is not valid, please check path input!"
+    exit
 }
